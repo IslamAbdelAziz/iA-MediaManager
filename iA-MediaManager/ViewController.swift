4cb473f1cce93e51ba5f97dd8b5e85c378d84717
iSlam <islam.abdelaziz.d@gmail.com>
2019-10-14 11:15:02 +0300

    Initial Commit

---
+++ iA-MediaManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iA-MediaManager
+//
+//  Created by iSlam on 10/14/19.
+//  Copyright © 2019 iSlamAbdel-Aziz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
